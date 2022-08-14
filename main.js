64a94a450ddf436d56b3d5c5ea070f7118dea05b
moripa115 <70812517+moripa115@users.noreply.github.com>
2022-08-14 17:41:22 +0900

    ver1.0.0

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 "use strict";
 
-navigator.geolocation.getCurrentPosition((position) => {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  fetch(
-    `https://cyberjapandata2.gsi.go.jp/general/dem/scripts/getelevation.php\?lon\=${lon}\&lat\=${lat}`
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data.elevation));
-});
+const bloccoli_btn = document.getElementById("bloccoli_btn");
+
+bloccoli_btn.addEventListener("click", () => {
+  document.getElementById("bloccoli").innerText = "...計算中"
+  navigator.geolocation.getCurrentPosition((position) => {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    fetch(
+      `https://cyberjapandata2.gsi.go.jp/general/dem/scripts/getelevation.php\?lon\=${lon}\&lat\=${lat}`
+    )
+      .then((response) => response.json())
+      .then((data) => document.getElementById("bloccoli").innerText = Math.floor(data.elevation / 0.08))
+  });
+})
